8597b74ac3a8bf672d5f626eae7967833e2178cc
a <a>
2019-08-19 14:01:40 -0300

    Using query and route params

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 
 const server = express();
 
-server.get("/teste", (req, res) => {
-  return res.json({ message: "Hello World" });
+// Query Params = ?teste=1
+// Route Params = /users/1
+// Request body = { "name": "Luiz", "email": "casimirofelisaki@gmail.com" }
+
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json({ message: `Buscando o usuario ${id}` });
 });
 
 server.listen(3000);
