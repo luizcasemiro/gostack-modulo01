121ff9838590e12a2372cfe3255adefc1a3acdef
a <a>
2019-08-19 14:15:12 -0300

    A simple CRUD using memory storage

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
 // Query Params = ?teste=1
 // Route Params = /users/1
 // Request body = { "name": "Luiz", "email": "casimirofelisaki@gmail.com" }
 
-server.get("/users/:id", (req, res) => {
-  const { id } = req.params;
-  return res.json({ message: `Buscando o usuario ${id}` });
+// CRUD = Create, Read, Update, Delete
+
+const users = ["Luiz Casemiro", "Ana", "Victor"];
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+  users[index] = name;
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1);
+  return res.send();
 });
 
 server.listen(3000);
